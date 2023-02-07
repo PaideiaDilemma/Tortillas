1a1ff46e69ba8c84e4abcbb5deff85292006b918
Maximilian Seidler <maximilian.seidler@student.tugraz.at>
2023-02-07 20:03:30 +0100

    fix: setup script bug

--- setup_sweb.sh
+++ setup_sweb.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-[ "$#" -eq 3 ] || die "usage: setup_sweb.sh <tortillas_path> <sweb_path> <example>"
+[ "$#" -eq 3 ] || (echo "usage: setup_sweb.sh <tortillas_path> <sweb_path> <example>" && exit 1)
 
 tortillas_path=$1
 sweb_path=$2
@@ -8,13 +8,13 @@ example_dir="$tortillas_path/examples/$3"
 # Patch sweb
 git -C $sweb_path apply --ignore-whitespace $tortillas_path/examples/base/sweb_patches/*.diff
 
-if ["$3" -ne "base"] && [ -d "$example_dir/sweb_patches" ]; then
+if [ "$3" != "base" ] && [ -d "$example_dir/sweb_patches" ]; then
     git -C $sweb_path apply --ignore-whitespace $example_dir/sweb_patches/*.diff
 fi
 
 # Copy tortillas_config.yml
-if ["$3" -ne "base"] && [ -f "$example_dir/tortillas_config.yml"]; then
+if [ -f "$example_dir/tortillas_config.yml" ]; then
     cp "$example_dir/tortillas_config.yml" $sweb_path/
 else
-    cp "$tortillas_path/examples/base/tortillas_config.yml" $sweb_path/tortillas_config.yml
+    cp "$tortillas_path/examples/base/tortillas_config.yml" $sweb_path/
 fi
