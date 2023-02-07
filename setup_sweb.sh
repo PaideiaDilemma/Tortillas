28651300b0a8a59b14e999d7aca9dca324da31b6
Maximilian Seidler <maximilian.seidler@student.tugraz.at>
2023-02-07 19:39:54 +0100

    examples: make all other examples inherit from base

--- setup_sweb.sh
+++ setup_sweb.sh
@@ -5,10 +5,16 @@ tortillas_path=$1
 sweb_path=$2
 example_dir="$tortillas_path/examples/$3"
 
-git -C $sweb_path apply --ignore-whitespace $tortillas_path/examples/common_sweb_patches/*.diff
+# Patch sweb
+git -C $sweb_path apply --ignore-whitespace $tortillas_path/examples/base/sweb_patches/*.diff
 
-if [ -d "$example_dir/sweb_patches" ]; then
+if ["$3" -ne "base"] && [ -d "$example_dir/sweb_patches" ]; then
     git -C $sweb_path apply --ignore-whitespace $example_dir/sweb_patches/*.diff
 fi
 
-cp $example_dir/tortillas_config.yml $sweb_path/
+# Copy tortillas_config.yml
+if ["$3" -ne "base"] && [ -f "$example_dir/tortillas_config.yml"]; then
+    cp "$example_dir/tortillas_config.yml" $sweb_path/
+else
+    cp "$tortillas_path/examples/base/tortillas_config.yml" $sweb_path/tortillas_config.yml
+fi
