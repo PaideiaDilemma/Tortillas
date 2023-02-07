310c633c438e61e455a5ee30359428fa5b9afb6f
Maximilian Seidler <maximilian.seidler@student.tugraz.at>
2023-02-07 15:12:30 +0100

    added an extended example

--- setup_sweb.sh
+++ setup_sweb.sh
@@ -1,8 +1,14 @@
-[ "$#" -eq 2 ] || die "usage: setup_sweb.sh <tortillas_path> <sweb_path>"
+#!/bin/bash
+[ "$#" -eq 3 ] || die "usage: setup_sweb.sh <tortillas_path> <sweb_path> <example>"
 
 tortillas_path=$1
 sweb_path=$2
+example_dir="$tortillas_path/examples/$3"
 
-git -C $sweb_path apply --ignore-whitespace $tortillas_path/sweb_patches/*.diff
+git -C $sweb_path apply --ignore-whitespace $tortillas_path/examples/common_sweb_patches/*.diff
 
-cp $tortillas_path/tortillas_config.yml $sweb_path/
+if [ -d "$example_dir/sweb_patches" ]; then
+    git -C $sweb_path apply --ignore-whitespace $example_dir/sweb_patches/*.diff
+fi
+
+cp $example_dir/tortillas_config.yml $sweb_path/
