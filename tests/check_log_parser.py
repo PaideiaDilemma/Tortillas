aa7a5a7ce92603f9edc9b332b014b7f9f5e88fa5
Maximilian Seidler <maximilian.seidler@student.tugraz.at>
2023-02-20 15:12:21 +0100

    tests: adapt tests to new config format

--- tests/check_log_parser.py
+++ tests/check_log_parser.py
@@ -1,12 +1,12 @@
 import logging
 from tortillas.log_parser import LogParser
-from tortillas.tortillas_config import ParseConfigEntry
+from tortillas.tortillas_config import AnalyzeConfigEntry
 
 
 def test_log_splitting():
-    config_entry = ParseConfigEntry(label='test',
-                                    scope='SYSCALL',
-                                    pattern=r'(.*)').compile_pattern()
+    config_entry = AnalyzeConfigEntry(name='test',
+                                      scope='SYSCALL',
+                                      pattern=r'(.*)').compile_pattern()
 
     log_parser = LogParser(log_file_path='./tests/assets/out_log.txt',
                            logger=logging.getLogger(),
@@ -14,15 +14,16 @@ def test_log_splitting():
 
     log_data = log_parser.parse()
 
-    assert config_entry.label in log_data
+    assert config_entry.name in log_data
     assert ('Syscall::EXIT: called, exit_code: 1237619379\n'
-            in log_data[config_entry.label])
+            in log_data[config_entry.name])
 
 
 def test_parsing_int_splitting():
-    config_entry = ParseConfigEntry(label='test',
-                                    scope='SYSCALL',
-                                    pattern=r'exit_code: (\d+)')
+    config_entry = AnalyzeConfigEntry(name='test',
+                                      scope='SYSCALL',
+                                      pattern=r'exit_code: (\d+)')
+
     config_entry.compile_pattern()
 
     log_parser = LogParser(log_file_path='./tests/assets/out_log.txt',
@@ -31,18 +32,18 @@ def test_parsing_int_splitting():
 
     log_data = log_parser.parse()
 
-    assert config_entry.label in log_data
-    assert '1237619379' in log_data[config_entry.label]
+    assert config_entry.name in log_data
+    assert '1237619379' in log_data[config_entry.name]
 
 
 def test_mutliple_config_entries():
     config = [
-            ParseConfigEntry(label='a', scope='SYSCALL',
-                             pattern='(.*)'),
-            ParseConfigEntry(label='b', scope='THREAD',
-                             pattern='(kill: (.*))'),
-            ParseConfigEntry(label='c', scope='PAGEFAULT',
-                             pattern=r'(Address:\s+0x[0-9a-fA-F]+)')]
+            AnalyzeConfigEntry(name='a', scope='SYSCALL',
+                               pattern='(.*)'),
+            AnalyzeConfigEntry(name='b', scope='THREAD',
+                               pattern='(kill: (.*))'),
+            AnalyzeConfigEntry(name='c', scope='PAGEFAULT',
+                               pattern=r'(Address:\s+0x[0-9a-fA-F]+)')]
 
     for entry in config:
         entry.compile_pattern()
@@ -54,7 +55,7 @@ def test_mutliple_config_entries():
     log_data = log_parser.parse()
 
     for entry in config:
-        assert entry.label in log_data.keys()
+        assert entry.name in log_data.keys()
 
     assert any('kill' in log for log in log_data['b'])
     assert 'Address:          0x8006000' in log_data['c']
--- tests/check_test_result.py
+++ tests/check_test_result.py
@@ -2,16 +2,11 @@ import logging
 
 from tortillas.log_parser import LogParser
 from tortillas.test_specification import TestSpec
-from tortillas.tortillas_config import (ParseConfigEntry,
-                                        AnalyzeConfigEntry)
+from tortillas.tortillas_config import AnalyzeConfigEntry
 from tortillas.test_result import TestResult
 
 
-def _get_log_data():
-    config_entry = ParseConfigEntry(label='test',
-                                    scope='SYSCALL',
-                                    pattern=r'(.*)').compile_pattern()
-
+def _get_log_data(config_entry: AnalyzeConfigEntry):
     log_parser = LogParser(log_file_path='./tests/assets/out_log.txt',
                            logger=logging.getLogger(),
                            config=[config_entry])
@@ -24,11 +19,13 @@ def _get_test_spec():
 
 
 def test_analzye_add_as_error():
-    log_data = _get_log_data()
-
-    config_entry = AnalyzeConfigEntry(label='test',
+    config_entry = AnalyzeConfigEntry(name='test',
+                                      scope='SYSCALL',
+                                      pattern=r'(.*)',
                                       mode='add_as_error',
-                                      status='PANIC')
+                                      status='PANIC').compile_pattern()
+
+    log_data = _get_log_data(config_entry)
 
     test_result = TestResult(test_repr='pytest',
                              test_spec=_get_test_spec(),
@@ -43,7 +40,9 @@ def test_analzye_add_as_error():
 def test_analzye_exit_codes():
     log_data = {'test': ['1', '2', '3', '4']}
 
-    config_entry = AnalyzeConfigEntry(label='test',
+    config_entry = AnalyzeConfigEntry(name='test',
+                                      scope='',
+                                      pattern='',
                                       mode='exit_codes',
                                       status='FAILED')
 
@@ -53,18 +52,20 @@ def test_analzye_exit_codes():
 
     test_result.analyze(log_data)
 
+    # test_result.errors:
+    # 5x Unexpected exit code {code}
+    # 1x Expected exit code(s) {codes}
     assert len(test_result.errors) == 5
-    '''5x Unexpected exit code {code}
-       1x Expected exit code(s) {codes}
-    '''
 
     assert test_result.status == TestResult.Status.FAILED
 
 def test_expect_stdout():
-    log_data = {'stdout': ['A'],
-                'expect': ['A', 'B']}
+    log_data = {'test': ['TORTILLAS EXPECT: A', 'A',
+                         'TORTILLAS EXPECT: B']}
 
-    config_entry = AnalyzeConfigEntry(label='expect',
+    config_entry = AnalyzeConfigEntry(name='test',
+                                      scope='',
+                                      pattern='',
                                       mode='expect_stdout',
                                       status='FAILED')
 
@@ -74,9 +75,9 @@ def test_expect_stdout():
 
     test_result.analyze(log_data)
 
+    # test_result.errors:
+    # 1x Expected ouput: B
+    # 1x Actual output: A
     assert len(test_result.errors) == 2
-    '''1x Expected ouput: B
-       1x Actual output: A
-    '''
 
     assert test_result.status == TestResult.Status.FAILED
