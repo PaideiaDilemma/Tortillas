636eb9efdeb371b042ccc8b7975ae69ece4f837c
Maximilian Seidler <maximilian.seidler@student.tugraz.at>
2023-03-02 18:42:53 +0100

    tests: adapt to path change

--- tests/check_tortillas_config.py
+++ tests/check_tortillas_config.py
@@ -2,12 +2,6 @@ from tortillas.tortillas_config import TortillasConfig
 
 
 def test_parse_base_config():
-    config = TortillasConfig('./examples/base/tortillas_config.yml')
-
-    assert config
-
-
-def test_parse_extended_config():
-    config = TortillasConfig('./examples/extended/tortillas_config.yml')
+    config = TortillasConfig('./examples/tortillas_config.yml')
 
     assert config
